41a71fd9507c61899acfab38bc7183c1e579e297
Lidia Angelica Ionita <lia.ionita@hotmail.com>
2018-10-25 23:38:47 +0200

    Update 2-bucle-for.js

--- 1-bucles/2-bucle-for.js
+++ 1-bucles/2-bucle-for.js
@@ -1,3 +1,9 @@
 // ej 2
 // haz un bucle que muestre por consola los impares del 3 al 17 inclusive
 // Utilizad el bucle for
+var i=0;
+for (i=13; i<18; i++) {
+    if (i%2 ==1 ) {
+        console.log(i);
+    }
+}
